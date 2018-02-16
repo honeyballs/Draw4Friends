cedf8312a2986feaeb5d6b33f37f07b46d276316
Unknown <yannick.bals@web.de>
2018-02-16 12:11:58 +0100

    Add Android project

---
+++ app/src/main/java/de/thm/draw4friends/LoginActivity.java
@@ -0,0 +1,12 @@
+package de.thm.draw4friends;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Yannick Bals on 16.02.2018.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+
+}
